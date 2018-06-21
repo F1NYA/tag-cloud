dce1f1ff5b18b702e2631b181e87608f6e1f7406
Triodin <ufolkin1@gmail.com>
2018-06-21 13:00:28 +0300

    miss

--- flask_app/app/messages_processor/vectorizer.py
+++ flask_app/app/messages_processor/vectorizer.py
@@ -4,7 +4,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 def create_vectorizer():
     return TfidfVectorizer()
 
-
+# dd
 def vectorize(tokenized_messages, vectorizer):
     print('Vectorizing ...')
     return vectorizer.fit_transform([word for word in tokenized_messages])
